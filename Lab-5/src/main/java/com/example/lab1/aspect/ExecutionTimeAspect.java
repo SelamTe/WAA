7f9a714ab48f343cb4fc750f8429576a1ae2239c
SelamTe <98483043+SelamTe@users.noreply.github.com>
2022-11-12 18:29:18 -0600

    Lab 5

---
+++ Lab-5/src/main/java/com/example/lab1/aspect/ExecutionTimeAspect.java
@@ -0,0 +1,27 @@
+package com.example.lab1.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ExecutionTimeAspect {
+
+    @Pointcut("@annotation(com.example.lab1.aspect.annotation.ExecutionTime)")
+
+    public void executionTimeAnnotation() {
+    }
+
+    @Around("executionTimeAnnotation()")
+    public Object calculateExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long start = System.nanoTime();
+        var result = joinPoint.proceed();
+        long finish = System.nanoTime();
+        System.out.println(joinPoint.getSignature().getName() + "takes the time of " + (finish - start) + "nanoseconds");
+        return result;
+    }
+}
---
+++ Lab-5/src/main/java/com/example/lab1/config/Override.java
@@ -0,0 +1,4 @@
+package com.example.lab1.config;
+
+public @interface Override {
+}
