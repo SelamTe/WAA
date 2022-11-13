6b98a53cca0179c2f222426ebea95fa45a8be4e8
SelamTe <98483043+SelamTe@users.noreply.github.com>
2022-11-12 18:04:43 -0600

    lab5

--- Lab-5/src/main/java/com/example/lab1/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package com.example.lab1.service;
-
-import com.example.lab1.domain.dto.request.LoginRequest;
-import com.example.lab1.domain.dto.response.LoginResponse;
-
-public interface AuthService {
-    LoginResponse login (LoginRequest longinRequest);
-}
